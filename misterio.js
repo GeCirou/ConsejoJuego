060e70554a5226540f0f0f961d1aab821e7cfa10
Ge Cirou <ca_gerardo@hotmail.com>
2022-09-13 17:03:24 -0300

    presentacion acomodada

--- misterio.js
+++ misterio.js
@@ -4,8 +4,8 @@ var vict2 = getElementById("v1");
 var investigador = "";
 var victima = [];
 var asesino = [];
-var herramienta = [Hacha, Navaja, Soga, Fogón, Pala, Lobato, Cucharón, Cajón de patrulla, Garrafa, Letrina];
-var lugar = [Hall, Sala de estar, Comedor, Cocina, Salón de baile, Jardín de invierno, Sala de billar, Biblioteca, Estudio];
+var herramienta = ["Hacha", "Navaja", "Soga", "Fogón", "Pala", "Lobato", "Cucharón", "Cajón de patrulla", "Garrafa", "Letrina"];
+var lugar = [Hall, "Sala de estar", "Comedor", "Cocina", "Salón de baile", "Jardín de invierno", "Sala de billar", "Biblioteca", "Estudio"];
 var solucion = "";
 
 class solucion{
@@ -18,10 +18,10 @@ class solucion{
 }
 
 if (vict1){
-    var victima = [Gato, Buey, Ardilla, Lince, Orca, Tiburón, Oso, Fonti];
-    var asesino = [Tero, Lorna, Búho, Pantera, Cigüeña, Edith, Llama, Claudia];
+    var victima = ["Gato", "Buey", "Ardilla", "Lince", "Orca", "Tiburón", "Oso", "Fonti"];
+    var asesino = ["Tero", "Lorna", "Búho", "Pantera", "Cigüeña", "Edith", "Llama", "Claudia"];
 } else if (vict2){
-    var asesino = [Gato, Buey, Ardilla, Lince, Orca, Tiburón, Oso, Fonti];
-    var victima = [Tero, Lorna, Búho, Pantera, Cigüeña, Edith, Llama, Claudia];
+    var asesino = ["Gato", "Buey", "Ardilla", "Lince", "Orca", "Tiburón", "Oso", "Fonti"];
+    var victima = ["Tero", "Lorna", "Búho", "Pantera", "Cigüeña", "Edith", "Llama", "Claudia"];
 }
 
